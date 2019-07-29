ea0d96c3531c004e3cf0077bab58047175c2ea50
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-29 16:37:07 -0700

    added localhost to client

--- client.js
+++ client.js
@@ -2,13 +2,15 @@ const net = require('net');
 const readline = require('readline');
 const { playMessage } = require('./lib/utils/sound');
 
+const host = process.argv[2] || 'localhost';
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: '> '
 });
 
-const client = net.createConnection(9999, () => {
+const client = net.createConnection(9999, host, () => {
   console.log('I am connected');
   
   rl.prompt();
