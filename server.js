a930ab5f4f0a8fac2fd484729e4abb296a335dc7
Dirt Deodara <thecrowleaps@gmail.com>
2019-07-29 10:45:46 -0700

    got our basic tiny server to work

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-require('./lib/utils/connect')();
+// require('dotenv').config();
+// require('./lib/utils/connect')();
 
-const app = require('./lib/app');
+const { app } = require('./lib/app');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 9999;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+  console.log(`Vigilantly listening on PORT ${PORT}`);
 });
---
+++ sound.js
@@ -0,0 +1,51 @@
+const player = require('play-sound')();
+
+const a = './src/assets/tones/a.wav';
+const b = './src/assets/tones/b.wav';
+const c = './src/assets/tones/c.wav';
+const d = './src/assets/tones/d.wav';
+const e = './src/assets/tones/e.wav';
+const f = './src/assets/tones/f.wav';
+const g = './src/assets/tones/g.wav';
+const h = './src/assets/tones/h.wav';
+const i = './src/assets/tones/i.wav';
+const j = './src/assets/tones/j.wav';
+const k = './src/assets/tones/k.wav';
+const l = './src/assets/tones/l.wav';
+const m = './src/assets/tones/m.wav';
+const n = './src/assets/tones/n.wav';
+const o = './src/assets/tones/o.wav';
+const p = './src/assets/tones/p.wav';
+const q = './src/assets/tones/q.wav';
+const r = './src/assets/tones/r.wav';
+const s = './src/assets/tones/s.wav';
+const t = './src/assets/tones/t.wav';
+const u = './src/assets/tones/u.wav';
+const v = './src/assets/tones/v.wav';
+const w = './src/assets/tones/w.wav';
+const x = './src/assets/tones/x.wav';
+const y = './src/assets/tones/y.wav';
+const z = './src/assets/tones/z.wav';
+
+const array = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+const play = (sound) => {
+  return new Promise((resolve, reject) => {
+    player.play(sound, (err) => {
+      if(err) {
+        console.log(err, 'error');
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+};
+
+const playSong = async(arr) => {
+  await arr.reduce(async(acc, beat) => [
+    ...await acc,
+    await play(beat)
+  ], await[]);
+};
+
+playSong(array);
