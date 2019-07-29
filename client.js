79a8fa0e08ba8d710cd95c69eaf2f1aaca5323d8
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-29 13:32:03 -0700

    we connected the client to the server

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> '
+});
+
+const client = net.createConnection(9999, () => {
+  console.log('I am connected');
+  
+  rl.prompt();
+  rl.on('line', line => {
+    client.write(line);
+    rl.prompt();
+  });
+});
+
+client.on('data', data => {
+  console.log(data.toString());
+  rl.prompt();
+});
