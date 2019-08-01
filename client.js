1952bcdeedabc50dd4b569509b72a8a3c3e43a80
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-01 11:28:18 -0700

    big refactor for yesterday messages, made text not required for yesterday

--- client.js
+++ client.js
@@ -23,42 +23,16 @@ const client = net.createConnection(9999, host, () => {
 
 const date = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-let yesterday = false;
-let yesterdaysMessages = [];
+
 
 client.on('data', data => {
   const dataToStr = data.toString();
   const dataObject = JSON.parse(dataToStr);
-  if(dataToStr.startsWith('%%%') && dataToStr.endsWith('$$$')) {
-    const messages = dataToStr.split('\n').slice(1);
-    messages.forEach(message => {
-      console.log(message);
-      yesterdaysMessages.push(message.split(': ')[1]);
+  if(dataObject instanceof Array) {
+    dataObject.forEach(message => {
+      console.log(`${message.nickname}: ${message.text}`);
     });
-    playMessage(yesterdaysMessages.join(' '));
-
-  } else if(dataToStr.startsWith('%%%')) {
-    yesterdaysMessages = []; 
-    yesterday = true;
-    const messages = dataToStr.slice(3).split('\n');
-    messages.forEach(message => {
-      console.log(message);
-      yesterdaysMessages.push(message.split(': ')[1]);
-    });
-
-  } else if(dataToStr.endsWith('$$$')) {
-    yesterday = false;
-    const messages = dataToStr.split('\n').slice(0, -1);
-    messages.forEach(message => {
-      console.log(message);
-      yesterdaysMessages.push(message.split(': ')[1]);
-    });
-    playMessage(yesterdaysMessages.join(' '));
-
-  } else if(yesterday){
-    console.log(dataToStr);
-    yesterdaysMessages.push(dataToStr.split(': ')[1]);
-    
+    playMessage(dataObject.map(message => message.text).join(' '));
   } else {
     console.log(`posted on: ${date}`);
     console.log(`${dataObject.nickname}: ${dataObject.text}`);
